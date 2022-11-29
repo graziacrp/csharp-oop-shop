067d1a87f0f2d69eb64c8d0f97df961b7e51a6b6
graziacrp <graziacarpineta25@gmail.com>
2022-11-29 17:05:47 +0100

    Aggiungere i file di progetto.

---
+++ CSharpShop/Prodotto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop
+{
+    public class Prodotto
+    {
+        //Proprietà
+        {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private double Iva;
+
+        //Costruttori (codice visibile solo in lettura)
+        public Prodotto (int codice, string nome, string descrizione, double prezzo, double Iva  )
+        {
+            //this.codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.Iva = Iva;
+        }
+
+        public double GetCodiceProdotto()
+        {
+            double codice;
+            Random random = new Random();
+            codice = random.Next(20, 20000);
+            return codice;
+        }
+
+        public double IvaProdotto ()
+        {
+            double Iva;
+            Iva = prezzo * 1.22;
+            return Iva;
+        }
+
+        public void StampaProdotto ()
+
+            Console.WriteLine("Il codice del prodotto è:" + nome +  getCodiceProdotto ());
+            Console.WriteLine("Il nome del prodotto è:" + nome);
+        Console.WriteLine ("Descrizione del prodotto è" + descrizione);
+            Console.WriteLine("Il prezzo del prodotto è" + prezzo + €);
+            Console.WriteLine("L'Iva del prodotto è" + Iva ());
+}
+
+
+
+
+
+   
---
+++ CSharpShop/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharpShop;
+
+
+
+
+Prodotto shop = new Prodotto("Fazzoletti", "doppio velo", 1);
+Console.WriteLine("Primo prodotto");
+shop.StampaProdotto();
+
+
+Prodotto shop2 = new Prodotto("Cioccolata", "Cioccolata bicolore", 2);
+Console.WriteLine("Secondo prodotto");
+shop2.StampaProdotto();
+
+Prodotto shop3 = new Prodotto("Pasta", "Orecchiette", 5.50);
+Console.WriteLine("Terzo prodotto");
+shop3.StampaProdotto();
+
+
+
